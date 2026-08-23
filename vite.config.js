a70a9f3ad2f16e9bccd5e08359ee32d353c97a51
Dima-Chuck <dmelehov9555@gmail.com>
2026-08-23 16:53:37 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const stylesPath = path.resolve(__dirname, 'src/styles');
 const REPO_NAME = '1817-family';
 
 export default defineConfig({
@@ -12,8 +13,13 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "variables" as *; @use "mixins" as *;`,
-        loadPaths: [path.resolve(__dirname, 'src/styles')],
+        loadPaths: [stylesPath],
+        additionalData(source, filePath) {
+          if (filePath.includes('_variables.scss') || filePath.includes('_mixins.scss')) {
+            return source;
+          }
+          return `@use "variables" as *; @use "mixins" as *;\n${source}`;
+        },
       },
     },
   },
